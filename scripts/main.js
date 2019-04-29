69a27ac11570b4092fb82afc196e9bd5c48a1e8e
oldvirginmary <wyuten@gmail.com>
2019-04-30 00:29:12 +0700

    Hot fix and build

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,9 @@
         })
 
         request.done(function(data, textStatus, jqXHR) {
+            // Make blog from data
             for (var i = 0; i < data.length; i++) {
-                var review = $('<div>').addClass('review col-3 card card-body');
+                var review = $('<div>').addClass('review col-sm-5 col-md-3 card card-body');
                 var title = $('<h3>').addClass('title card-title').text(data[i].title);
                 var content = $('<p>').addClass('content card-text').text(data[i].content);
                 var time = $('<p>').addClass('time card-text');
@@ -26,31 +27,32 @@
                 results.append(review);
             }
 
-            // Add input form as a review
+            // Add input form as a post block with Bootstrap 4
+            // Block
             var review = $('<div>').attr('id', 'review-form');
             results.append(review);
-            $('#review-form').addClass('review col-3 card card-body');
-
+            $('#review-form').addClass('review col-sm-5 col-md-3 card card-body');
+            // Title
             var inputTitle = $('<input>').attr('id', 'input-title');
             review.append(inputTitle);
             $('#input-title').addClass('card-title form-control');
             $('#input-title').attr('type', 'text').attr('name', 'title');
-
+            // Text area
             var inputContent = $('<textarea>').attr('id', 'input-content');
             review.append(inputContent);
             $('#input-content').addClass('card-text form-control');
             $('#input-content').attr('name', 'content').attr('rows', '2');
-
+            // Button
             var button = $('<button>').attr('id', 'send-btn');
             review.append(button);
             $('#send-btn').addClass('btn').text('send');
 
+            // Append to template
             results.append(review);
-            //
         })
 
         request.fail(function(jqXHR, textStatus, errorThrown) {
-            console.log('request fail');
+            alert('request fail');
         })
     }
 
@@ -72,21 +74,25 @@
         request.done(function(data, textStatus, jqXHR) {
             var results = $('.reviews');
 
-            var review = $('<div>').addClass('review row card card-body');
+            // Delete form
+            var reviewForm = $('#review-form')
+            $('#input-title').val('');
+            $('#input-content').val('');
+            reviewForm.remove()
+
+            // Make new post
+            var review = $('<div>').addClass('review col-sm-5 col-md-3 card card-body');
             var title = $('<h3>').addClass('title card-title').text(titleField);
             var content = $('<p>').addClass('content card-text').text(contentField);
             var time = $('<p>').addClass('time card-text');
             time.append($('<span>').addClass('text-muted').text('now'));
 
+            // Add all
             review.append(title);
             review.append(content);
             review.append(time);
             results.append(review);
-
-            $('#input-title').val('');
-            $('#input-content').val('');
-
-            alert('done!');
+            results.append(reviewForm);
         })
 
         request.fail(function(jqXHR, textStatus, errorThrown) {
@@ -98,10 +104,6 @@
 
     $(document).ready(function() {
         getReviews();
-        // $('#send-btn').click(function() {
-        //     console.log('post')
-        //     postReview();
-        // })
         $('.reviews').on('click', '#send-btn', function() {
             postReview();
         })
